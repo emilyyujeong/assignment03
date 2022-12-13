fb967fc090aace325d74ee91c6544b6050b102c9
emilyyujeong <118698609+emilyyujeong@users.noreply.github.com>
2022-12-13 17:47:05 +0900

    Add files via upload

---
+++ js/secc.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.mainSlide').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+
+    });
+});
